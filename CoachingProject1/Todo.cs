bcf202f57f6bfa845bde778b2688be7ddd27b390
Tim Musgrove <tim.musgrove@stepstone.com>
2023-05-11 10:29:52 +0100

    init

---
+++ CoachingProject1/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace CoachingProject1;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
---
+++ CoachingProject1/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoachingProject1;
+
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> option)
+        : base(option) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
